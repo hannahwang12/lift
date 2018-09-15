75bc633e45fae0ce1f19b716b88a3ece4971ea01
Hannah Wang <hannahwang12@gmail.com>
2018-09-15 15:12:04 -0400

    count reps

--- app/index.js
+++ app/index.js
@@ -7,18 +7,40 @@ import { Accelerometer } from "accelerometer";
 
 let start = document.getElementById("start");
 let stop = document.getElementById("stop");
-// demotext.text = "Fitbit Studio rocks!";
 
+const range = 0.5;
+const top = 4;
+const bot = -4;
+let reach_top = false;
+let reach_bot = false;
+var velocity = 0
+var displacement = 0
 var accel = new Accelerometer({ frequency: 10 });
+var reps = 0;
 accel.onreading = function() {
-  console.log(JSON.stringify(accel));
-  console.log(accel);
+//  console.log(accel.x - 9.81);
+//  velocity += (accel.x - 9.81) / 100
+//  console.log(velocity)
+  
+  let accelx = accel.x;
+  if (accelx > top) reach_top = true;
+  if (accelx < bot) reach_bot = true;
+  if (reach_top && reach_bot && Math.abs(accelx) < range) {
+    reps++;
+    reach_top = false;
+    reach_bot = false;
+  }
+  console.log("accel: " + accelx)
+  console.log(reps);
+  // if (accelx > -0.1 && accelx < 0.1) accelx = 0
+  // displacement += 0.5 * accelx * 0.01
+  // console.log(displacement * 100)
 
   // Peek the current sensor values
-  console.log("ts:", accel.timestamp,
-              "x:", accel.x,
-              "y:", accel.y,
-              "z:", accel.z);
+  // console.log("ts:", accel.timestamp,
+  //             "x:", accel.x,
+  //             "y:", accel.y,
+  //             "z:", accel.z);
 
 }
 
