7414e38270f478834e9234f06ee45507a3f27ddc
Ariel Lam <ariel.lam8@gmail.com>
2018-09-16 06:38:53 -0400

    Added UI flow, new pages

--- app/index.js
+++ app/index.js
@@ -9,7 +9,17 @@ import { peerSocket } from "messaging";
 let repsCount = document.getElementById("reps");
 let counting = false;
 let container = document.getElementById("panorama");
-
+let quickstart = document.getElementById("quickstart");
+let arms = document.getElementById("arms");
+let legs = document.getElementById("legs");
+let settings = document.getElementById("settings");
+let arms_rep = document.getElementById("arms_rep");
+let arms_list = document.getElementById("arms_title");
+let legs_list = document.getElementById("legs_list");
+let arms_biceps = document.getElementById("arms_biceps");
+let btnBR = document.getElementById("btn-br");
+let btnwrap = document.getElementById("btnwrap");
+let stop = document.getElementById("btn-br-stop");
 const range = 0.8;
 const top = 2.5;
 const bot = -2.5;
@@ -37,19 +47,53 @@ accel.onreading = function() {
   console.log(reps);
 }
 
+btnBR.onclick = function(e) {
+  count();
+}
+
+stop.onclick = function(e) {
+  count();
+}
+
 document.onkeypress = function(e) {
   console.log("Key pressed: " + e.key);
   if (e.key == 'up') count();
+  if (e.key == 'back'){
+  }
+}
+
+function toggle(ele) {
+  ele.style.display = (ele.style.display === "inline") ? "none" : "inline";
+}
+
+quickstart.onclick = function(e) {
+  toggle(container)
+  // toggle(quickstart)
+}
+
+arms.onclick = function(e) {
+  toggle(container)
+  toggle(arms_list)
+}
+
+arms_biceps.onclick = function(e) {
+  toggle(arms_list)
+}
+
+legs.onclick = function(e) {
+  console.log("click");
 }
 
 function count() {
   if (!counting) {
     console.log('start')
+    toggle(btnwrap)
     accel.start();
     counting = true;
     
   } else {
     console.log('stop')
+    toggle(btnwrap)
     accel.stop();
     counting = false;
     if (peerSocket.readyState === peerSocket.OPEN) {
